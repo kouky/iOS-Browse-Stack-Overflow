f7f2212ae1cd4d1f5f0bf2d5078ec60431d57628
Michael Koukoullis <mkoukoullis@gmail.com>
2014-02-08 11:45:58 +1100

    Initial BrowseOverflowViewController

---
+++ BrowseOverflow/BrowseOverflowViewController.h
@@ -0,0 +1,13 @@
+//
+//  BrowseOverflowViewController.h
+//  BrowseOverflow
+//
+//  Created by Michael Koukoullis on 8/02/2014.
+//  Copyright (c) 2014 Michael Koukoullis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BrowseOverflowViewController : UIViewController
+
+@end
---
+++ BrowseOverflow/BrowseOverflowViewController.m
@@ -0,0 +1,38 @@
+//
+//  BrowseOverflowViewController.m
+//  BrowseOverflow
+//
+//  Created by Michael Koukoullis on 8/02/2014.
+//  Copyright (c) 2014 Michael Koukoullis. All rights reserved.
+//
+
+#import "BrowseOverflowViewController.h"
+
+@interface BrowseOverflowViewController ()
+
+@end
+
+@implementation BrowseOverflowViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
